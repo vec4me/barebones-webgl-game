85fe99cb90652c5df751e3425133c49f1a10ae92
Jeffrey Skinner <jeff@je.gy>
2024-02-13 01:08:59 -0800

    this shouldve been like this from the beginning... i suck at git

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-printf '' > main.html
-printf '<script>' >> main.html
-cat main.js >> main.html
-printf '</script>' >> main.html
+clang main.c --no-standard-libraries --target=wasm32 -Wl,--export-all -Wl,--no-entry -Wl,-undefined,suppress --output - | base64 --wrap=0 -
+#clang -O3 -c -emit-llvm main.c -o main.bc
+#llc -O3 main.bc -o main.s
+#wasm-ld main.s -o main.wasm --no-entry --export-all
--- wasm/build.sh
+++
@@ -1,4 +0,0 @@
-clang main.c --no-standard-libraries --target=wasm32 -Wl,--export-all -Wl,--no-entry -Wl,-undefined,suppress --output - | base64 --wrap=0 -
-#clang -O3 -c -emit-llvm main.c -o main.bc
-#llc -O3 main.bc -o main.s
-#wasm-ld main.s -o main.wasm --no-entry --export-all
